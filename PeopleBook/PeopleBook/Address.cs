387853587e1541cf8b5525afce04414143315772
atsuko_c <atsukosgit@gmail.com>
2019-12-12 14:55:14 -0700

    re-added cs files

---
+++ PeopleBook/PeopleBook/Address.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace PeopleBook
+{
+    public class Address
+    {
+        private int id;
+        private string street;
+        private string state;
+        private string zip;
+        private string country;
+
+        // constructors
+
+        public Address() { }
+
+        public Address(int id, string street, string state, string zip, string country)
+        {
+            this.id = id;
+            this.street = street;
+            this.state = state;
+            this.zip = zip;
+            this.country = country;
+        }
+
+        // getters
+
+    }
+}
