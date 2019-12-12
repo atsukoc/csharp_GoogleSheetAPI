bd62455c3c653580630ce635055925322160beeb
atsuko_c <atsukosgit@gmail.com>
2019-12-12 14:53:53 -0700

    added Program.cs

---
+++ PeopleBook/PeopleBook/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PeopleBook
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+        }
+    }
+}
