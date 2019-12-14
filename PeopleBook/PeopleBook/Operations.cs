2a01c21996631cbd2533e20ec23f2e6230a3f4a3
atsuko_c <atsukosgit@gmail.com>
2019-12-14 14:13:25 -0700

    now the app can ask for user input

---
+++ PeopleBook/PeopleBook/Operations.cs
@@ -0,0 +1,81 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace PeopleBook
+{
+    public class Operations
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
+        static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+        static string ApplicationName = "My first dot net project";
+        static String SpreadsheetId = "1_fY6TqTeUETqQR8zEks1mA1TpgsRBgkgJWp2KZtBPpQ";
+        static String SheetName = "Sheet1";
+
+        public static void WriteData()
+        {
+
+        }
+
+
+        public static void ReadData()
+        {
+            UserCredential credential;
+
+            using (var stream =
+            new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Sheets API service.
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define request parameters.
+            String range = String.Format("{0}!A2:H", SheetName);
+            SpreadsheetsResource.ValuesResource.GetRequest request =
+                    service.Spreadsheets.Values.Get(SpreadsheetId, range);
+
+            // Prints the values from the spreadsheet
+
+            ValueRange response = request.Execute();
+            IList<IList<Object>> values = response.Values;
+
+            if (values != null && values.Count > 0)
+            {
+                foreach (var row in values)
+                {
+                    Console.WriteLine("ID: {0} | {1} {2}", row[0], row[1], row[2]);
+                    
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data found.");
+            }
+
+           
+        }
+    }
+    
+}
--- PeopleBook/PeopleBook/Program.cs
+++ PeopleBook/PeopleBook/Program.cs
@@ -10,67 +10,131 @@ using System.Threading;
 
 namespace PeopleBook
 {
-    class Program
+    public class Program
     {
-        // If modifying these scopes, delete your previously saved credentials
-        // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
-        static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
-        static string ApplicationName = "My first dot net project";
-
         static void Main(string[] args)
         {
-            UserCredential credential;
-
-            using (var stream =
-                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            Boolean on = true;
+           
+            while (on)
             {
-                // The file token.json stores the user's access and refresh tokens, and is created
-                // automatically when the authorization flow completes for the first time.
-                string credPath = "token.json";
-                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
-                    Scopes,
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore(credPath, true)).Result;
-                Console.WriteLine("Credential file saved to: " + credPath);
-            }
+                char input = askInputs();
 
-            // Create Google Sheets API service.
-            var service = new SheetsService(new BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = ApplicationName,
-            });
-
-            /* Define request parameters.
-             * Columns A = id, B = First Name, C = Last Name, D = Street Address, E = State
-             * F = Zip, G = Email, H = Country
-             */
-            String spreadsheetId = "";
-            String sheetName = "Sheet1";
-            String range = String.Format("{0}!A2:H", sheetName);
-            SpreadsheetsResource.ValuesResource.GetRequest request =
-                    service.Spreadsheets.Values.Get(spreadsheetId, range);
-
-            // Prints the values from the spreadsheet
-            
-            ValueRange response = request.Execute();
-            IList<IList<Object>> values = response.Values;
-
-            if (values != null && values.Count > 0)
-            { 
-                foreach (var row in values)
+                if(input.Equals('r') || input.Equals('R'))
                 {
-                    
-                    Console.WriteLine("{0}, {1} {2}", row[0], row[1], row[2]);
+                    Operations.ReadData();
+                }
+                else if(input.Equals('x') || input.Equals('X'))
+                {
+                    on = false;
+                    Console.WriteLine("Bye");
+                    Environment.Exit(0);
+                }
+                else if(input.Equals('i') || input.Equals('I'))
+                {
+                    string first_name;
+                    string last_name;
+                    string street_address;
+                    string state;
+                    string zip;
+                    string country;
+                    string email;
+
+                    // Gather information
+                    askFirstName(out first_name);
+                    askLastName(out last_name);
+                    askStreetAddress(out street_address);
+                    askState(out state);
+                    askZip(out zip);
+                    askCountry(out country);
+                    askEmail(out email);
+
+                    // Display the entered information
+                    Console.WriteLine(String.Format("\n{0} {1}", first_name, last_name));
+                    Console.WriteLine(String.Format("{0}, {1}, {2} {3}", street_address, state, zip, country));
+                    Console.WriteLine(String.Format("Email: {0}\n", email));
+
+                    // Ask if anything needs to be modified before inserting to the google sheet
+                    Console.WriteLine("Is all correct? Enter Y for YES, N for NO");
+                    var response = Console.ReadLine();
+                    char charResponse = Convert.ToChar(response);
+
+                    if (charResponse.Equals('Y') || charResponse.Equals('y'))
+                    {
+                        Console.WriteLine("Person added to the spread sheet");
+                    }
+                    else if(charResponse.Equals('N') || charResponse.Equals('n'))
+                    {
+                        Console.WriteLine("What do we need to fix?");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong input");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input");
                 }
             }
-            else
-            {
-                Console.WriteLine("No data found.");
-            }
-            Console.Read();
+            
+        }
+
+        static private char askInputs()
+        {
+            Console.WriteLine("\n=========== Menu ==========");
+            Console.WriteLine("R | Read the entire sheet");
+            Console.WriteLine("I | Add a new person to the sheet");
+            Console.WriteLine("X | Exit");
+            Console.WriteLine("=======================");
+            var input = Console.ReadLine();
+            char charInput = Convert.ToChar(input);
+            return charInput;
+        }
+
+        static private void askFirstName(out string firstname)
+        {
+            Console.WriteLine("First name?");
+            firstname = Console.ReadLine();
+        }
+
+        static private void askLastName(out string lastname)
+        {
+            Console.WriteLine("Last name?");
+            lastname = Console.ReadLine();
+        }
+
+        static private void askStreetAddress(out string street)
+        {
+            Console.WriteLine("Street address?");
+            street = Console.ReadLine();
+        }
+
+        static private void askState(out string state)
+        {
+            Console.WriteLine("State?");
+            state = Console.ReadLine();
+        }
+
+        static private void askZip(out string zip)
+        {
+            Console.WriteLine("Zip Code?");
+            zip = Console.ReadLine();
+        }
+
+        static private void askCountry(out string country)
+        {
+            Console.WriteLine("Country?");
+            country = Console.ReadLine();
+        }
+
+        static private void askEmail(out string email)
+        {
+            Console.WriteLine("Email?");
+            email = Console.ReadLine();
         }
     }
+
+
+    
 }
